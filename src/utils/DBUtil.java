992d73ce8d06f906167f90e32b733cdee8085533
warapon <koki.fujiwara1208@gmail.com>
2019-11-14 01:08:11 +0900

    Add getAllTasks

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -1,10 +1,10 @@
-package DBUtil;
+package utils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class utils {
+public class DBUtil {
     private static final String PERSISTENCE_UNIT_NAME = "tasklist";
     private static EntityManagerFactory emf;
 
